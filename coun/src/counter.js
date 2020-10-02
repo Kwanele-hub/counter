ecde0ebcb4847481d3c34f7bde3c80eea5ec3cac
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-02 08:16:12 -0400

    i added the decrement button and called it function

--- coun/src/counter.js
+++ coun/src/counter.js
@@ -3,15 +3,19 @@ import React, { useState } from "react"
 export default function Count() {
     const [count, setCount] = useState(0);
 
-   const  handleClick = ()=>{
+   const  add = ()=>{
         setCount(count + 1)
+    }
+    const reduce =()=>{
+        setCount(count - 3)
     }
 
     return (
         <div>
             <p>Counter</p>
             <p>{count}</p>
-            <button onClick={() => handleClick()}>Click Me</button>
+            <button onClick={() => add()}>Increment</button>
+            <button onClick={() => reduce()}>Decrement</button>
         </div>
     )
 }
