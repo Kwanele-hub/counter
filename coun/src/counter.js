b95e9fa8e729e68b95d46a77f8fe4e7dc6bde9d6
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-02 07:25:11 -0400

    add my handle function, also added the paragraph

--- coun/src/counter.js
+++ coun/src/counter.js
@@ -1,10 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 
-export default function Counter(){
-    
-    return(
+export default function Count() {
+    const [count, setCount] = useState(0);
+
+   const handleClick = ()=>{
+        setCount(count + 1)
+    }
+
+    return (
         <div>
             <p>Counter</p>
+            <p>{count}</p>
+            <button onClick={() => handleClick()}>Click Me</button>
         </div>
     )
 }
